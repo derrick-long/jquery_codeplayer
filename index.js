97d800e6c1f402abaed3bff8d7f96fa0e990be20
derrick-long <long.derrick8@gmail.com>
2017-12-28 12:52:12 -0500

    add jquery for updating panels

--- index.js
+++ index.js
@@ -10,4 +10,11 @@ $(".toggleButton").click(function (){
   $(this).removeClass("highlightedButton");
 });
 
-$("textarea").height($(window).height() - $("#header").height() - 15);
+$(".panel").height($(window).height() - $("#header").height() - 15);
+
+$(".panel").width(($(window).width() / 2) - 10);
+
+
+$("textarea").on('change keyup paste', function(){
+  $("#outputPanel").contents().find('html').html($('#htmlPanel').val());
+});
