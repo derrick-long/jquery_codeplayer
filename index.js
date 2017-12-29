d77d1376ac6c8219e8cfd8a3d0915418adbb881e
derrick-long <long.derrick8@gmail.com>
2017-12-28 22:06:44 -0500

    Add other panels and make sizing responsive using jquery

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 $(".toggleButton").hover(function (){
   $(this).addClass("highlightedButton");
 },function (){
@@ -7,12 +8,24 @@ $(".toggleButton").hover(function (){
 
 $(".toggleButton").click(function (){
   $(this).toggleClass("selected");
+
   $(this).removeClass("highlightedButton");
+
+  var panelId = $(this).attr("id") + "Panel";
+
+  $("#" + panelId).toggleClass("hidden");
+
+   var activePanels = 4 - $('.hidden').length;
+
+  $(".panel").width(($(window).width() / activePanels) - 10);
 });
+
 
 $(".panel").height($(window).height() - $("#header").height() - 15);
 
 $(".panel").width(($(window).width() / 2) - 10);
+
+
 
 
 $("textarea").on('change keyup paste', function(){
