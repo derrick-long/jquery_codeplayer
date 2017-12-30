f4d8d2466a9c14d2246e36cd6b12c70d8aedd7f4
derrick-long <long.derrick8@gmail.com>
2017-12-30 11:47:50 -0500

    Add javascript panel function, DRY up code

--- index.js
+++ index.js
@@ -21,15 +21,20 @@ $(".toggleButton").click(function (){
 });
 
 
+function updateOutput() {
+  $("iframe").contents().find("html").html("<html> <head> <style type= 'text/css'>" +
+  $("#cssPanel").val() + "</style> </head> <body>" +
+  $("#htmlPanel").val() + "</body> </html>");
+  document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+}
+
 $(".panel").height($(window).height() - $("#header").height() - 15);
 
 $(".panel").width(($(window).width() / 2) - 10);
 
-
+updateOutput();
 
 
 $("textarea").on('change keyup paste', function(){
-  $("iframe").contents().find("html").html("<html> <head> <style type= 'text/css'>" +
-  $("#cssPanel").val() + "</style> </head> <body>" +
-  $("#htmlPanel").val() + "</body> </html>");
+  updateOutput();
 });
